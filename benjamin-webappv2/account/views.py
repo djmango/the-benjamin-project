bb6e2b20b34da249baa336220b4054e463aa48e1
Sulaiman Ghori <skghori03@gmail.com>
2017-12-26 04:55:14 -0500

    finished basic sql login, guild list WIP

--- benjamin-webappv2/account/views.py
+++ benjamin-webappv2/account/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse, HttpRequest
 from requests_oauthlib import OAuth2Session
 import os
 import requests
+import MySQLdb
+
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-
 API_ENDPOINT = 'https://discordapp.com/api/v6'
 CLIENT_ID = '393872294004391936'
 CLIENT_SECRET = 'a3d9gVHQPdXo-n73rVskNAPJCjNh8w0p'
@@ -13,7 +14,8 @@ REDIRECT_URI = 'http://localhost:8000/account/callback'
 TOKEN_URL = 'https://discordapp.com/api/oauth2/token'
 API_BASE_URL = 'https://discordapp.com/api'
 AUTHORIZATION_BASE_URL = 'https://discordapp.com/api/oauth2/authorize'
-
+mysql = MySQLdb.Connection(host='35.196.94.58', user='root',
+                           passwd='uEgwrOugbAG0Nbb1', port=3306, db='testv1')
 
 def token_updater(token):
     os.environ['oauth2_token'] = token
@@ -42,7 +44,6 @@ def login(request):
     os.environ['oauth2_state'] = state
     return redirect(authorization_url)
 def callback(request):
-    #TODO: store info in sql, username, token, id
     discord = make_session(state=os.environ['oauth2_state'])
     token = discord.fetch_token(
         TOKEN_URL,
@@ -52,4 +53,6 @@ def callback(request):
                      headers={"Authorization": "Bearer %s" % token['access_token']})
     userInfo = r.json()
     print(userInfo['username'])
+    mysql.query("""INSERT INTO account_account (username, discriminator, avatar, token, guilds) VALUES ('%s', %s, '%s', '%s', '%s')""" % (userInfo['username'], userInfo['discriminator'], userInfo['avatar'], token['access_token'], 'nullfornow'))
+    mysql.commit()
     return HttpResponse("congrats, you did it! now, if you are me, do the TODO stuff")
